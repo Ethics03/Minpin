05733de29cadc024f68299279322b998bd1e89de
Rachit Srivastava <honey032004@gmail.com>
2025-03-07 23:39:47 +0530

    refactor: fixed db connection

--- db/db.go
+++ db/db.go
@@ -1,35 +1,53 @@
-package main
+package db
 
 import (
 	"context"
 	"fmt"
 	"os"
 
-	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
 )
 
-var Conn *pgx.Conn
+var pool *pgxpool.Pool
 
-func main() {
+func InitDB() error {
 
-	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	err := godotenv.Load()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
-		os.Exit(1)
+		fmt.Println(".env not set")
 	}
-	defer conn.Close(context.Background())
+
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		return fmt.Errorf("DB URL not set")
+	}
+
+	fmt.Println("Connecting to database:", dbURL)
+
+	pool, err = pgxpool.New(context.Background(), dbURL)
+	if err != nil {
+		return fmt.Errorf("failed to connect to database: %w", err)
+	}
+
+	err = pool.Ping(context.Background())
+	if err != nil {
+		return fmt.Errorf("database is not reachable: %w", err)
+	}
+
+	fmt.Println("Database connected successfully")
+	return nil
 }
 
-// InsertURL saves a new URL into the database
-func InsertURL(tag, longURL, shortURL string) error {
+func InsertURL(ctx context.Context, tag, longURL, shortURL string) error {
 	query := `INSERT INTO urls (tag, long_url, short_url) VALUES ($1, $2, $3) ON CONFLICT (tag) DO NOTHING`
-	_, err := Conn.Exec(context.Background(), query, tag, longURL, shortURL)
+	_, err := pool.Exec(ctx, query, tag, longURL, shortURL)
 	return err
 }
 
-func GetLongURL(shortURL string) (string, error) {
+func GetLongURL(ctx context.Context, shortURL string) (string, error) {
 	var longURL string
-	err := Conn.QueryRow(context.Background(), "SELECT long_url FROM urls WHERE short_url=$1", shortURL).Scan(&longURL)
+	err := pool.QueryRow(ctx, "SELECT long_url FROM urls WHERE short_url=$1", shortURL).Scan(&longURL)
 	if err != nil {
 		return "", err
 	}
@@ -37,5 +55,8 @@ func GetLongURL(shortURL string) (string, error) {
 }
 
 func CloseDB() {
-	Conn.Close(context.Background())
+	if pool != nil {
+		pool.Close()
+		fmt.Println("DB CLOSED")
+	}
 }
