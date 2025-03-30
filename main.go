4742408c46cf1d2a0b228f015656a919ee80417a
Rachit Srivastava <honey032004@gmail.com>
2025-03-30 23:02:18 +0530

    feat: handlers used

--- main.go
+++ main.go
@@ -1,95 +1,28 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"minpin/db"
-	"minpin/url"
+	"minpin/handlers"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
-
-// request
-type shortenREQ struct {
-	URL string `json:"url"`
-	Tag string `json:"tag"`
-}
-
-// response
-type shortenRESP struct {
-	Message  string `json:"message"`
-	ShortURL string `json:"short_url"`
-}
 
 func main() {
 
 	if err := db.InitDB(); err != nil {
-		log.Fatalf("Failed to initialize database: %v\n", err)
+		log.Fatalf("Failed to initialize db: %v\n", err)
 	}
+
 	defer db.CloseDB()
 
-	router := http.NewServeMux()
+	r := chi.NewRouter()
 
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodGet {
-			http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-			return
-		}
-		w.Write([]byte("URL Shortener API is running"))
-	})
-
-	router.HandleFunc("/shorten", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-			return
-		}
-
-		var req shortenREQ
-		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-			http.Error(w, "Invalid request body", http.StatusBadRequest)
-			return
-		}
-		defer r.Body.Close()
-
-		shortURL, err := url.ShortURL(r.Context(), req.Tag, req.URL)
-		if err != nil {
-			http.Error(w, "Failed to shorten URL", http.StatusInternalServerError)
-			return
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(shortenRESP{
-			Message:  "URL shortened successfully",
-			ShortURL: shortURL,
-		})
-	})
-
-	router.HandleFunc("/resolve", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodGet {
-			http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-			return
-		}
-
-		shortURL := r.URL.Query().Get("short")
-		if shortURL == "" {
-			http.Error(w, "Short URL parameter missing", http.StatusBadRequest)
-			return
-		}
-
-		longURL, err := url.ResolveURL(r.Context(), shortURL)
-		if err != nil {
-			http.Error(w, "Short URL not found", http.StatusNotFound)
-			return
-		}
-
-		http.Redirect(w, r, longURL, http.StatusFound)
-	})
-
-	server := http.Server{
-		Addr:    ":3000",
-		Handler: router,
-	}
+	r.Post("/shorten", handlers.ShortenHandler)
+	r.Get("/{shortURL}", handlers.RedirectHandler)
 
 	fmt.Println("Server is running on port 3000")
-	log.Fatal(server.ListenAndServe())
+	log.Fatal(http.ListenAndServe(":3000", r))
 }
